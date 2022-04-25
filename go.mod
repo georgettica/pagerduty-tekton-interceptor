95a20435ee608a36d5e33434c444db638801aa6b
Ron Green <11993626+georgettica@users.noreply.github.com>
2022-04-25 11:03:51 +0300

    add CI

--- go.mod
+++ go.mod
@@ -1,6 +1,6 @@
 module github.com/georgettica/pagerduty-tekton-interceptor
 
-go 1.18
+go 1.17
 
 require (
 	github.com/tektoncd/triggers v0.19.1
--- main.go
+++ main.go
@@ -4,13 +4,11 @@ import (
 	"context"
 	"net/http"
 
+	triggersv1 "github.com/tektoncd/triggers/pkg/apis/triggers/v1beta1"
+	"github.com/tektoncd/triggers/pkg/interceptors"
 	"go.uber.org/zap"
 	"google.golang.org/grpc/codes"
 	corev1lister "k8s.io/client-go/listers/core/v1"
-
-	"github.com/tektoncd/triggers/pkg/apis/triggers/v1beta1"
-	triggersv1 "github.com/tektoncd/triggers/pkg/apis/triggers/v1beta1"
-	"github.com/tektoncd/triggers/pkg/interceptors"
 )
 
 var _ triggersv1.InterceptorInterface = (*Interceptor)(nil)
@@ -27,9 +25,9 @@ func NewInterceptor(sl corev1lister.SecretLister, l *zap.SugaredLogger) *Interce
 	}
 }
 
-// PagerDutyInterceptor provides a webhook to intercept and pre-process events
+// PagerDutyInterceptor provides a webhook to intercept and pre-process events.
 type PagerDutyInterceptor struct {
-	SecretRef *v1beta1.SecretRef `json:"secretRef,omitempty"`
+	SecretRef *triggersv1.SecretRef `json:"secretRef,omitempty"`
 	// +listType=atomic
 	EventTypes []string `json:"eventTypes,omitempty"`
 }
@@ -46,12 +44,15 @@ func (w *Interceptor) Process(ctx context.Context, r *triggersv1.InterceptorRequ
 	if p.EventTypes != nil {
 		actualEvent := http.Header(r.Header).Get("X-Event-Key")
 		isAllowed := false
+
 		for _, allowedEvent := range p.EventTypes {
 			if actualEvent == allowedEvent {
 				isAllowed = true
+
 				break
 			}
 		}
+
 		if !isAllowed {
 			return interceptors.Failf(codes.FailedPrecondition, "event type %s is not allowed", actualEvent)
 		}
@@ -63,19 +64,18 @@ func (w *Interceptor) Process(ctx context.Context, r *triggersv1.InterceptorRequ
 		if p.SecretRef.SecretKey == "" {
 			return interceptors.Fail(codes.FailedPrecondition, "bitbucket interceptor secretRef.secretKey is empty")
 		}
+
 		header := headers.Get("X-Hub-Signature")
 		if header == "" {
 			return interceptors.Fail(codes.InvalidArgument, "no X-Hub-Signature header set")
 		}
+
 		ns, _ := triggersv1.ParseTriggerID(r.Context.TriggerID)
+
 		_, err := interceptors.GetSecretToken(nil, w.SecretLister, p.SecretRef, ns)
 		if err != nil {
 			return interceptors.Failf(codes.FailedPrecondition, "error getting secret: %v", err)
 		}
-
-		//if err := gh.ValidateSignature(header, []byte(r.Body), secretToken); err != nil {
-		//	return interceptors.Failf(codes.FailedPrecondition, err.Error())
-		//}
 	}
 
 	return &triggersv1.InterceptorResponse{
